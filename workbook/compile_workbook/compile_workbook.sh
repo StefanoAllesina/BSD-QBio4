bbd994a2f3213bc57ef74f8b03785292fb0d0b1d
Stefano Allesina <sallesina@uchicago.edu>
2018-08-13 08:40:15 -0500

    Recompiled workbook: - schedule missing alternative for Gemma - double check new tutorials and workshops - all activities start on odd page

--- workbook/compile_workbook/compile_workbook.sh
+++ workbook/compile_workbook/compile_workbook.sh
@@ -25,11 +25,14 @@ pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 ## Tutorials cover # Revised
 pdftk tmp.pdf ../other_docs/cover_tutorials.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
+## insert blank page
+pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+
 ## Microscopy and ImageJ # Revised
 pdftk tmp.pdf ../../tutorials/microscopy_and_imageJ/data/ImageProcessingExercises.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## insert blank page
-pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+#pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Basic I # Revised
 pdftk tmp.pdf ../../tutorials/basic_computing_1/code/basic_computing_1.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
@@ -52,6 +55,9 @@ pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 ## Advanced II # Revised
 pdftk tmp.pdf ../../tutorials/advanced_computing_2/code/advanced_computing_2.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf 
 
+## insert blank page
+pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+
 ## Defensive programming # Revised
 pdftk tmp.pdf ../../tutorials/defensive_programming/code/defensive_programming.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
@@ -62,8 +68,7 @@ pdftk tmp.pdf ../../tutorials/data_visualization/handout.pdf output tmp2.pdf; mv
 pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Reproducibility
-## MISSING!!
-#pdftk tmp.pdf ../../tutorials/reproducibility/code/reproducibility.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+pdftk tmp.pdf ../../tutorials/reproducibility/code/reproducibility.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## insert blank page
 pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
@@ -87,26 +92,31 @@ pdftk tmp.pdf ../../workshops/meren/code/MBL_QBio_Meren.pdf output tmp2.pdf; mv 
 pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Palmer
-## MISSING!!
-#pdftk tmp.pdf ../../workshops/yang/NGS_analysis_tutorial.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+pdftk tmp.pdf ../../workshops/sepalmer/code/mt_lip_computation.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Steinruecken # Revised
 pdftk tmp.pdf ../../workshops/steinruecken/code/MBL_WorkshopJN.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
+## Redistill to reduce size
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=../workbook.pdf tmp.pdf
+## remove tmp
+rm tmp.pdf
 
-## Introductory math tutorials cover # Revised
-#pdftk tmp.pdf ../other_docs/cover_math_intros.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+## Math refresher in separate file
 
-## Stochastic # Revised
-#pdftk tmp.pdf ../other_docs/intro_stochastic_processes_with_solutions.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+## Introductory math tutorials cover 
+cp ../other_docs/cover_math_intros.pdf tmp.pdf
+
+# Stochastic # Revised
+pdftk tmp.pdf ../other_docs/intro_stochastic_processes_with_solutions.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Dynamical # Revised
-#pdftk tmp.pdf ../other_docs/intro_dynamical_systems_with_solutions.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
+pdftk tmp.pdf ../other_docs/intro_dynamical_systems_with_solutions.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## insert blank page
 #pdftk tmp.pdf ../other_docs/blank.pdf output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Redistill to reduce size
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=../workbook.pdf tmp.pdf
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=../math_refresher.pdf tmp.pdf
 ## remove tmp
 rm tmp.pdf
