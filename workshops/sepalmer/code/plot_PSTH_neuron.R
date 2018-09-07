2a2bb7900bcb878f9a04c8a1cfd1e41aa33a0bd1
wjj <williamjjohnston@gmail.com>
2018-09-07 10:27:32 -0400

    added in a single direction example and updated direction tuning plotting function to take category boundary as an optional input

--- workshops/sepalmer/code/plot_PSTH_neuron.R
+++ workshops/sepalmer/code/plot_PSTH_neuron.R
@@ -23,8 +23,9 @@ plot_PSTH_neuron <- function(filename,filter_size){
                          each=length(xs)),
                  spk_rates=flat_means)
   
-  ggplot(d, aes(x=ts, y=spk_rates, color=factor(dir))) +
+  fig <- ggplot(d, aes(x=ts, y=spk_rates, color=factor(dir))) +
     geom_line(aes(group=factor(dir)), size=1) +
     labs(x='time from stimulus onset (ms)', y='spks/s', 
          color='motion direction (degrees)')
+  fig
 }
